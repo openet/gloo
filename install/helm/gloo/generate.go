900f275a4eae5ba64f7b6e42604f145f923fae64
rahulp <rahul.pampati@openet.com>
2021-09-03 19:59:51 +0530

    Istio and Gloo integration

--- install/helm/gloo/generate.go
+++ install/helm/gloo/generate.go
@@ -107,7 +107,7 @@ func generateChartYaml(version string) error {
 		return err
 	}
 
-	chart.Version = "1.5.27-OPENET-SNAPSHOT"
+	chart.Version = "1.5.27-OPENET"
 
 	return writeYaml(&chart, chartOutput)
 }
