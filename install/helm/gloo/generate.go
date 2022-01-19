d2bebb3b4aa71050ed9a410c4a42fe62b9853ab3
Chin Kang <chinkang.tan@openet.com>
2022-01-19 19:07:09 +0800

    The build script set the version, remove hardcoded

--- install/helm/gloo/generate.go
+++ install/helm/gloo/generate.go
@@ -107,7 +107,7 @@ func generateChartYaml(version string) error {
 		return err
 	}
 
-	chart.Version = "1.9.7-OPENET-SNAPSHOT"
+	chart.Version = version
 
 	return writeYaml(&chart, chartOutput)
 }
