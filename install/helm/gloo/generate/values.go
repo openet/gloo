bfba487fb3f334aecb2b63ce1e3b4cce72cb9217
Jenny Shu <28537278+jenshu@users.noreply.github.com>
2022-07-02 05:06:12 -0400

    Parameterize the gateway proxy sidecar image [1.11] (#6670)
    * Parametrize the gateway proxy sidecar image
    
    * Updated the description for envoySidecarImage
    
    * Updated the description for envoySidecarImage
    
    * cl and test
    
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: Jenny Shu <jenny.shu@solo.io>
    
    Co-authored-by: Rinor Maloku <rinor.maloku@solo.io>
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>

--- install/helm/gloo/generate/values.go
+++ install/helm/gloo/generate/values.go
@@ -597,7 +597,8 @@ type Mtls struct {
 	Enabled               *bool                 `json:"enabled,omitempty" desc:"Enables internal mtls authentication"`
 	Sds                   SdsContainer          `json:"sds,omitempty"`
 	EnvoySidecar          EnvoySidecarContainer `json:"envoy,omitempty"`
-	EnvoySidecarResources *ResourceRequirements `json:"envoySidecarResources,omitempty" desc:"Sets default resource requirements for all envoy sidecar containers."`
+	IstioProxy            IstioProxyContainer   `json:"istioProxy,omitempty" desc:"Istio-proxy container"`
+	EnvoySidecarResources *ResourceRequirements `json:"envoySidecarResources,omitempty" desc:"Sets default resource requirements for all Envoy sidecar containers."`
 	SdsResources          *ResourceRequirements `json:"sdsResources,omitempty" desc:"Sets default resource requirements for all sds containers."`
 	IPv4Only              bool                  `json:"ipv4Only" desc:"This field if set false will bind the IPv6 address on envoy-sidecar-config."`
 }
@@ -608,6 +609,10 @@ type SdsContainer struct {
 
 type EnvoySidecarContainer struct {
 	Image *Image `json:"image,omitempty"`
+}
+
+type IstioProxyContainer struct {
+	Image *Image `json:"image,omitempty" desc:"Istio-proxy image to use for mTLS"`
 }
 
 type IstioSDS struct {
