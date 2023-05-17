d8d90faa7431c2e5a23bfd08abc51066281dbc32
Tiberiu Carp <tiberiu.carp@openet.com>
2023-05-17 11:36:07 +0100

    Change to next snapshot version.

--- install/helm/gloo/generate.go
+++ install/helm/gloo/generate.go
@@ -108,7 +108,7 @@ func generateChartYaml(version string) error {
 		return err
 	}
 
-	chart.Version = "1.13.18-OPENET-SNAPSHOT"
+	chart.Version = "1.13.18-OPENET-SNAPSHOT-2"
 
 	return writeYaml(&chart, chartOutput)
 }
