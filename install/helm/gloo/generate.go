96cd2669bcfe44d90ec23565b072da2e19b7599a
royprad <147032827+royprad@users.noreply.github.com>
2023-11-24 11:35:48 +0530

    Update generate.go

--- install/helm/gloo/generate.go
+++ install/helm/gloo/generate.go
@@ -108,7 +108,7 @@ func generateChartYaml(version string) error {
 		return err
 	}
 
-	chart.Version = "1.14.26-OPENET-SNAPSHOT-9"
+	chart.Version = "1.14.26-OPENET-SNAPSHOT-10"
 
 	return writeYaml(&chart, chartOutput)
 }
